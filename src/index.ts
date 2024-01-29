f668100b8d42332a249d9b19c4260fd3f54635fe
David Woolsey <woolsey2316@gmail.com>
2024-01-29 13:59:59 +1100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./enitites/Post";
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        entities: [Post],
+        dbName: 'lireddit',
+        type: 'postgresql',
+        debug: !__prod__
+    })
+    
+    const post = orm.em.create(Post, { title: 'my first post' })
+    await orm.em.persistAndFlush(post)
+
+}
+
+main().catch((err) => {
+    console.log(err)
+})
